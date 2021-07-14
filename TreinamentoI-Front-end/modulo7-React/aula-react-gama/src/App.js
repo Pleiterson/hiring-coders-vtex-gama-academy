645c12fea62fc33d8929855b8625d7540ef43779
pleiterson <pleiterson@gmail.com>
2021-07-14 20:58:15 -0300

    modulo7: concluido a aula 4 consumindo dados de uma api

--- TreinamentoI-Front-end/modulo7-React/aula-react-gama/src/App.js
+++ TreinamentoI-Front-end/modulo7-React/aula-react-gama/src/App.js
@@ -1,11 +1,22 @@
 import React, {useState} from 'react';
+import axios from 'axios';
 
 function App(props) {
-  const [usuario, setUsuario] = useState('Pleiterson');
+  const [usuario, setUsuario] = useState('');
+
+  function handlePesquisa() {
+    // console.log(usuario); // imprime o valor do input
+    // response.data pega apenas os repositórios do GitHub
+    axios.get(`https://api.github.com/users/${usuario}/repos`).then(response => console.log(response.data));
+  }
+
   return (
     <>
-      <input name="usuario" id="usuario" className="usuarioInput" placeholder="Usuário"/>
-      <button type="button">Pesquisar</button>
+      {/* onChange pega o que foi digitado no input e inclui no value através do useState */}
+      <input className="usuarioInput" placeholder="Usuário" value={usuario} onChange={e => setUsuario(e.target.value)}/>
+
+      {/* onclick chama a função handlePesquisa */}
+      <button type="button" onClick={handlePesquisa}>Pesquisar</button>
     </>
   );
 }
